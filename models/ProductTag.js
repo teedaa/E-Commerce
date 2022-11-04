6ec68bd186c2fee0c3a49ce3d44625b3c0b82217
teedaa <pengtedachengllhs@gmail.com>
2022-11-03 21:16:36 -0700

    Create product model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    }
     // define columns
   },
   {
