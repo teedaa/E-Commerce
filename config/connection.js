1180a442964ea2672ba16b254a12a844049319e8
teedaa <pengtedachengllhs@gmail.com>
2022-11-04 22:23:54 -0700

    undo tests

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,11 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-      port: process.env.PORT || 3001
     });
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
+// import sequelize connection
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
 app.use(routes);
 
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
